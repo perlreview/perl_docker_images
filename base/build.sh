dbeda1d7ad63e4354c14445b8f9f91edeb6c1a0a
brian d foy <briandfoy@pobox.com>
2025-02-09 06:28:15 -0500

    $TAG variable and push on success

--- base/build.sh
+++ base/build.sh
@@ -3,10 +3,12 @@
 COMMIT=$(git rev-parse HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 NAME=base
-VERSION=1.0.2
+VERSION=1.0.3
+TAG=perlreview/$NAME:$VERSION
 
 # https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
-docker build . -t perlreview/$NAME:$VERSION \
+docker build . -t $TAG \
 	--label="org.opencontainers.image.revision=$COMMIT" \
 	--label="org.opencontainers.image.created=$DATE" \
-	--label="version=$VERSION"
+	--label="version=$VERSION" \
+	&& docker push $TAG
