ca6b443d42f3048b13c8b6e9ca9bd0b3d42e8136
brian d foy <briandfoy@pobox.com>
2025-02-09 06:29:28 -0500

    $BASE_TAG, $TAG, $PERL_VERSION vars, and push on success

--- perl-5.40/build.sh
+++ perl-5.40/build.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+BASE_TAG=1.0.3
 COMMIT=$(git rev-parse HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 PERL_VERSION=5.40.1
 NAME=perl-$PERL_VERSION
 VERSION=1.0.0
+TAG=perlreview/$NAME:$VERSION
 
 # https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
-docker build . -t perlreview/$NAME:$VERSION \
+docker build . -t $TAG \
 	--label="org.opencontainers.image.description='Perl $PERL_VERSION with extras'" \
 	--label="org.opencontainers.image.revision=$COMMIT" \
 	--label="org.opencontainers.image.created=$DATE" \
-	--label="version=$VERSION"
+	--label="version=$VERSION" \
+	--build-arg BASE_TAG=$BASE_TAG \
+	--build-arg PERL_VERSION=$PERL_VERSION \
+	&& docker push $TAG
