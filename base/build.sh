97e76d50c5114740fb254d811d279fafdfedcd75
brian d foy <briandfoy@pobox.com>
2025-02-09 06:13:08 -0500

    Don't change the default user so later layers can use root

--- base/build.sh
+++ base/build.sh
@@ -3,7 +3,7 @@
 COMMIT=$(git rev-parse HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 NAME=base
-VERSION=1.0.1
+VERSION=1.0.2
 
 # https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
 docker build . -t perlreview/$NAME:$VERSION \
