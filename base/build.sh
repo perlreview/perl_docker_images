4b92a1c0da1b561bfd7e88db2cc19ff3b0a409a7
brian d foy <briandfoy@pobox.com>
2025-02-09 06:05:43 -0500

    move version to build.sh

--- base/build.sh
+++ base/build.sh
@@ -2,7 +2,11 @@
 
 COMMIT=$(git rev-parse HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+NAME=base
+VERSION=1.0.1
 
-docker build . -t perlreview/base:1.0 \
+# https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
+docker build . -t perlreview/$NAME:$VERSION \
 	--label="org.opencontainers.image.revision=$COMMIT" \
-	--label="org.opencontainers.image.created=$DATE"
+	--label="org.opencontainers.image.created=$DATE" \
+	--label="version=$VERSION"
