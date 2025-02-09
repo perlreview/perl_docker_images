175f60a80c7bdea8a9812c7a8130427a3b871d3d
brian d foy <briandfoy@pobox.com>
2025-02-09 09:01:01 -0500

    bump base version

--- base/build.sh
+++ base/build.sh
@@ -3,7 +3,7 @@
 COMMIT=$(git rev-parse HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 NAME=base
-VERSION=1.0.3
+VERSION=1.0.4
 TAG=perlreview/$NAME:$VERSION
 
 # https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
