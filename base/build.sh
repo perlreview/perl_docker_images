c1fe27ad77500158c3087ffeb38fd248cd31e456
brian d foy <briandfoy@pobox.com>
2025-03-22 13:56:31 -0400

    Turn some things in the base Dockerfile into variables.

--- base/build.sh
+++ base/build.sh
@@ -2,13 +2,25 @@
 
 COMMIT=$(git rev-parse HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+DOCKER_HUB_ACCOUNT=perlreview
 NAME=base
 VERSION=1.0.4
-TAG=perlreview/$NAME:$VERSION
+IMAGE_NAME=$DOCKER_HUB_ACCOUNT/$NAME
+TAG=$IMAGE_NAME:$VERSION
+LATEST_TAG=$IMAGE_NAME:latest
+MAINTAINER=briandfoy@pobox.com
+AUTHOR=$MAINTAINER
+USERNAME=perlreview
 
 # https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
-docker build . -t $TAG \
+docker build . \
+	-t $TAG \
+	-t $LATEST_TAG \
 	--label="org.opencontainers.image.revision=$COMMIT" \
 	--label="org.opencontainers.image.created=$DATE" \
 	--label="version=$VERSION" \
-	&& docker push $TAG
+	--build-arg AUTHOR=${AUTHOR} \
+	--build-arg MAINTAINER=${MAINTAINER} \
+	--build-arg USERNAME=${USERNAME} \
+	--build-arg VENDOR=${VENDOR} \
+	&& docker push $IMAGE_NAME --all-tags
