d9dfd57179d5021726e0861ff3d7f9b320dc2ecf
brian d foy <briandfoy@pobox.com>
2025-03-22 13:55:44 -0400

    A perl program to do everything for all versions

---
+++ Dockerfile
@@ -0,0 +1,61 @@
+ARG BASE_NAME
+ARG BASE_TAG
+ARG DOCKER_HUB_ACCOUNT
+
+FROM ${DOCKER_HUB_ACCOUNT}/${BASE_NAME}:${BASE_TAG}
+ARG BUILDKIT_SBOM_SCAN_STAGE=true
+
+ARG BASE_NAME
+ARG BASE_TAG
+ARG DOCKER_HUB_ACCOUNT
+ARG PERL_ARCHIVE_DIGEST
+ARG PERL_URL
+ARG PERL_URL_BASENAME
+ARG PERL_VERSION
+ARG REPO_DIR
+ARG USERNAME
+
+LABEL org.opencontainers.image.authors="briandfoy@pobox.com" \
+	org.opencontainers.image.title="Base image for https://github.com/perlreview/perl_docker_images" \
+	org.opencontainers.image.licenses="Artistic-2.0" \
+	org.opencontainers.image.url="https://github.com/perlreview/perl_docker_images" \
+	org.opencontainers.image.source="https://github.com/perlreview/perl_docker_images" \
+	org.opencontainers.image.vendor="The Perl Review"
+
+RUN cpan -T Devel::PatchPerl \
+	&& cd /tmp \
+	&& echo PERL_VERION ${PERL_VERSION} \
+	&& curl -s -fLO ${PERL_URL} \
+	&& echo "${PERL_ARCHIVE_DIGEST} ${PERL_URL_BASENAME}"  \
+	&& echo "${PERL_ARCHIVE_DIGEST} *${PERL_URL_BASENAME}" | sha256sum --strict --check - \
+	&& tar -xzf perl-${PERL_VERSION}.tar.gz \
+	&& cd perl-${PERL_VERSION} \
+	&& patchperl \
+	&& ./Configure -des -Dprefix=/usr/local \
+	&& make install \
+	&& cd \
+	&& rm -rf /tmp/* \
+	&& /usr/local/bin/cpan -T \
+		App::Cpan \
+		App::cpanminus \
+		Devel::Cover \
+		ExtUtils::MakeMaker \
+		HTTP::Tiny \
+		IO::Socket::SSL \
+		LWP \
+		LWP::Protocol::https \
+		Term::ReadLine \
+		Test::CPAN::Changes \
+		Test::Exception \
+		Test::Manifest \
+		Test::More \
+		Test::Pod \
+		Test::Pod::Coverage \
+		Test::Output \
+		YAML \
+		XML::Entities \
+	&& rm -rf /root/.cpan
+
+USER ${USERNAME}
+
+CMD [ "/usr/local/bin/perl", "-de0" ]
---
+++ build_images.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+use v5.30;
+use utf8;
+use strict;
+use warnings;
+
+use JSON::PP qw(decode_json);
+use Mojo::File;
+use Mojo::Util qw(dumper);
+
+my %args = (
+	account       => 'perlreview',
+	commit        => do { `git rev-parse HEAD` =~ s/\s+\z//r },
+	date          => do { `date -u +"%Y-%m-%dT%H:%M:%SZ"` =~ s/\s+\z//r },
+	image_version => do {
+		my( $minute, $hour, $day, $month, $year ) = (localtime)[1..5];
+		sprintf '%4d%02d%02d.%02d%02d', $year + 1900, $month, $day, $hour, $minute;
+		},
+	base_name     => 'base',
+	base_tag      => 'latest',
+	repo_dir      => 'https://github.com/perlreview/perl_docker_images',
+	username      => 'perlreview',
+	);
+
+my $version_info = do {
+	my $version_data_file = Mojo::File->new('data/checksums.json');
+	my $json = $version_data_file->slurp;
+	decode_json( $json )->{data};
+	};
+
+my $latest = do {
+	my $version_data_file = Mojo::File->new('data/latest.json');
+	my $json = $version_data_file->slurp;
+	my $data = decode_json( $json )->{data};
+
+	[
+		map { join '.', ( 5, $data->{$_}->@[-2,-1] ) }
+		grep { $_ % 2 == 0 }
+		keys $data->%*
+	]
+	};
+
+foreach my $version ( $latest->@* ) {
+	next unless $version eq '5.40.1';
+	my $compression = 'gz';
+	my $info = $version_info->{$version}{$compression};
+	next if $info->{minor} % 2;
+
+	say dumper($info);
+
+	$args{perl_version} = $version;
+	$args{name}         = "perl-$version";
+	$args{tag}          = "$args{account}/$args{name}:$args{image_version}";
+	$args{latest_tag}   = "$args{account}/$args{name}:latest";
+	$args{digest}       = $info->{sha256};
+	$args{perl_url}     = $info->{url};
+	$args{perl_url_basename} = $args{perl_url} =~ s|.*/||r;
+
+	build_image( \%args );
+	last;
+	}
+
+# https://www.docker.com/blog/generate-sboms-with-buildkit/
+sub build_image {
+	my( $args ) = @_;
+
+	say STDERR dumper($args);
+
+	local $ENV{BUILDKIT_PROGRESS} = 'plain';
+
+	my @command = ( qw(docker buildx build . -t), $args->{tag} );
+	push @command,
+		q(-t), $args->{tag},
+		q(-t), $args->{latest_tag},
+		qw(--progress plain),
+		q(--sbom=true),
+		q(--label),     qq(org.opencontainers.image.authors='Perl $args->{perl_version} with extras'),
+		q(--label),     qq(org.opencontainers.image.description='Perl $args->{perl_version} with extras'),
+		q(--label),     qq(org.opencontainers.image.revision=$args->{commit}),
+		q(--label),     qq(org.opencontainers.image.created=$args->{date}),
+		q(--build-arg), qq(DOCKER_HUB_ACCOUNT=$args->{account}),
+		q(--build-arg), qq(BASE_NAME=$args->{base_name}),
+		q(--build-arg), qq(BASE_TAG=$args->{base_tag}),
+		q(--build-arg), qq(PERL_ARCHIVE_DIGEST=$args->{digest}),
+		q(--build-arg), qq(PERL_VERSION=$args->{perl_version}),
+		q(--build-arg), qq(PERL_URL=$args->{perl_url}),
+		q(--build-arg), qq(PERL_URL_BASENAME=$args->{perl_url_basename}),
+		q(--build-arg), qq(REPO_DIR=$args->{repo_dir}),
+		q(--build-arg), qq(USERNAME=$args->{username}),
+		q(--push);
+
+	my $rc = system @command;
+
+	unless( $rc == 0 ) {
+		warn "docker build failed: $!\n";
+		warn dumper($args);
+		warn dumper(\@command);
+		return;
+		}
+
+	$rc = system( qw(docker push), "$args->{account}/$args->{name}", q(--all-tags) );
+	}
