f7d95c5edba26b79bf197834aad9d0d6ee98cd89
brian d foy <briandfoy@pobox.com>
2025-03-24 14:01:10 -0400

    still trying various builds.

--- base/build.sh
+++ base/build.sh
@@ -4,18 +4,20 @@ COMMIT=$(git rev-parse HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 DOCKER_HUB_ACCOUNT=perlreview
 NAME=base
-VERSION=1.0.4
+VERSION=1.0.5
 IMAGE_NAME=$DOCKER_HUB_ACCOUNT/$NAME
 TAG=$IMAGE_NAME:$VERSION
 LATEST_TAG=$IMAGE_NAME:latest
-MAINTAINER=briandfoy@pobox.com
-AUTHOR=$MAINTAINER
+AUTHOR=briandfoy@pobox.com
 USERNAME=perlreview
+BASE_IMAGE=debian:bookworm-slim
 
 # https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
 docker build . \
 	-t $TAG \
 	-t $LATEST_TAG \
+	--progress=plain \
+	--sbom=true \
 	--label="org.opencontainers.image.revision=$COMMIT" \
 	--label="org.opencontainers.image.created=$DATE" \
 	--label="version=$VERSION" \
@@ -23,4 +25,5 @@ docker build . \
 	--build-arg MAINTAINER=${MAINTAINER} \
 	--build-arg USERNAME=${USERNAME} \
 	--build-arg VENDOR=${VENDOR} \
-	&& docker push $IMAGE_NAME --all-tags
+	--build-arg BASE_IMAGE=${BASE_IMAGE} \
+	--push
