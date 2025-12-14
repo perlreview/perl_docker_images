af9c5688fd8e17e7e8233edb21c5bcec4436eb0a
brian d foy <briandfoy@pobox.com>
2025-12-14 05:02:27 -0500

    handle versions in order

--- layers/perl-base/build-perl-base-images.pl
+++ layers/perl-base/build-perl-base-images.pl
@@ -48,7 +48,7 @@ my @versions = do {
 	else            { $latest->@* }
 };
 
-VERSION: foreach my $version ( @versions ) {
+VERSION: foreach my $version ( sort { $a <=> $b } @versions ) {
 	unless( exists $version_info->{$version} ) {
 		warn "Do not have settings for <$version>. Skipping.\n";
 		next VERSION;
