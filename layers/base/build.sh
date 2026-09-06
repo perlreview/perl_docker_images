0be3c43509e6960f38dbca555b4d6d9a6573917a
brian d foy <briandfoy@pobox.com>
2026-09-06 05:09:24 -0400

    try the refactored base creation

--- layers/base/build.sh
+++ layers/base/build.sh
@@ -1,17 +1,59 @@
 #!/bin/sh
 
-REGISTRY=ghcr.io
+find_up() {
+  local target="$1"
+  local max_levels="${2:-10}"
+  local dir="$PWD"
+  local level=0
+
+  while [[ "$dir" != "/" && "$level" -le "$max_levels" ]]; do
+    if [[ -e "$dir/$target" ]]; then
+      echo "$dir/$target"
+      return 0
+    fi
+    if [[ -e "$dir/.git" ]]; then
+      return 1
+    fi
+    dir="$(dirname "$dir")"
+    ((level++))
+  done
+  return 1
+}
+
+SETTINGS_FILE=settings.json
+SETTINGS_PATH=$(find_up $SETTINGS_FILE 5)
+
+if [[ ! -f "$SETTINGS_PATH" ]]; then
+  echo "File not found: $SETTINGS_FILE" >&2
+  exit 1
+fi
+SECTION=base
+
+AUTHORS=$(jq -r .authors $SETTINGS_PATH)
+BASE_IMAGE=$(jq -r --arg s "$SECTION" '.[$s].image' $SETTINGS_PATH)
 COMMIT=$(git rev-parse HEAD)
+DESCRIPTION="The base layer that includes everything common among the Perl images from The Perl Review"
 DATE=$(date -u +"%Y%m%d.%H%M%S")
-GITHUB_ACCOUNT=perlreview
-NAME=base
-VERSION=$DATE
-IMAGE_NAME=$REGISTRY/$GITHUB_ACCOUNT/$NAME
-TAG=$IMAGE_NAME:$VERSION
+GITHUB_ORG_ACCOUNT=$(jq -r .github_org_account $SETTINGS_PATH)
+GITHUB_USERNAME=$(jq -r .github_username $SETTINGS_PATH)
 LATEST_TAG=$IMAGE_NAME:latest
-AUTHOR=briandfoy@pobox.com
-USERNAME=briandfoy
-BASE_IMAGE=debian:bookworm-slim
+LICENSES=$(jq -r .licenses $SETTINGS_PATH)
+NAME=$(jq -r --arg s "$SECTION" '.[$s].name' $SETTINGS_PATH)
+REGISTRY=$(jq -r .registry $SETTINGS_PATH)
+REPO=$(jq -r .repo_dir $SETTINGS_PATH)
+REPO_RAW_BASE="$REPO/blob/$COMMIT"
+SOURCE="$REPO_RAW_BASE/layers/base/Dockerfile"
+TITLE=$(jq -r --arg s "$SECTION" '.[$s].title' $SETTINGS_PATH)
+VENDOR=$(jq -r .vendor ../../settings.json)
+URL=https://github.com/orgs/$GITHUB_ORG_ACCOUNT/packages/container/package/base
+VENDOR=$(jq -r .vendor $SETTINGS_PATH)
+VERSION=$DATE
+
+
+IMAGE_NAME="$REGISTRY/$GITHUB_ORG_ACCOUNT/$NAME"
+LATEST_TAG=$IMAGE_NAME:latest
+README_URL="$REPO_RAW_BASE/README.md"
+TAG=$IMAGE_NAME:$VERSION
 
 # https://www.docker.com/blog/docker-best-practices-using-tags-and-labels-to-manage-docker-image-sprawl/
 docker buildx build . \
@@ -20,11 +62,16 @@ docker buildx build . \
 	--platform linux/amd64,linux/arm64,linux/386 \
 	--progress=plain \
 	--sbom=true \
-	--label="org.opencontainers.image.revision=$COMMIT" \
+	--label="org.opencontainers.image.authors='$AUTHORS'" \
 	--label="org.opencontainers.image.created=$DATE" \
-	--label="version=$VERSION" \
-	--build-arg AUTHOR=${AUTHOR} \
-	--build-arg USERNAME=${USERNAME} \
-	--build-arg VENDOR=${VENDOR} \
-	--build-arg BASE_IMAGE=${BASE_IMAGE} \
-	--push
+	--label="org.opencontainers.image.description='$DESCRIPTION'" \
+	--label="org.opencontainers.image.documentation=$README_URL" \
+	--label="org.opencontainers.image.licenses='$LICENSES'" \
+	--label="org.opencontainers.image.revision=$COMMIT" \
+	--label="org.opencontainers.image.source=$SOURCE" \
+	--label="org.opencontainers.image.title='$TITLE'" \
+	--label="org.opencontainers.image.url=$URL" \
+	--label="org.opencontainers.image.vendor='$VENDOR'" \
+	--label="org.opencontainers.image.version=$VERSION" \
+	--build-arg BASE_IMAGE=${BASE_IMAGE}
+#	--push
